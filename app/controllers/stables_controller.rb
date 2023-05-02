e1f86260ac6fe5a06ee8d1967f19c92ca2fccda2
Janan Akkad <jananakkad@hotmail.co.uk>
2023-05-02 14:37:08 +0100

    updated horses_attributes

--- app/controllers/stables_controller.rb
+++ app/controllers/stables_controller.rb
@@ -52,7 +52,7 @@ class StablesController < ApplicationController
     end
 
     def stable_params
-        params.require(:stable).permit(:name, horse_attributes:[
+        params.require(:stable).permit(:name, horses_attributes:[
             :name
         ])        
     end
--- app/models/stable.rb
+++ app/models/stable.rb
@@ -6,9 +6,11 @@ class Stable < ApplicationRecord
 
     before_save :make_title_case
 
-    def horse_attributes=(horse_name) #expecting this to be a hash
-        self.horse = Horse.find_or_create_by(name: horse_name)
-    end
+    accepts_nested_attributes_for :horses
+
+    # def horse_attributes=(horse_name) #expecting this to be a hash
+    #     self.horse = Horse.find_or_create_by(name: horse_name)
+    # end
 
     private
 
